5ae3d71ce5ee4b24b266e3f855dcbf072aeb11d2
Otaking <otaking424@gmail.com>
2026-09-04 21:56:28 -0700

    Added 3rd Ground Combo to Player

--- Hack_N_Slash/Source/Hack_N_Slash/Enums/ETargetingStyle.h
+++ Hack_N_Slash/Source/Hack_N_Slash/Enums/ETargetingStyle.h
@@ -13,9 +13,9 @@ UENUM(BlueprintType)
 enum class ETargetingStyle : uint8
 {
 	None UMETA(DisplayName = "None"),
-	AlignCam UMETA(DisplayName = "Based on camera direction alignment"),
-	AlignMove UMETA(DisplayName = "Based on move direction alignment"),
-	AlignMoveOrCam UMETA(DisplayName = "Based on NON-ZERO move direction alignment or camera direction alignment"),
-	AlignMoveOrDist UMETA(DisplayName = "Based on NON-ZERO move direction alignment or distance"),
-	Dist UMETA(DisplayName = "Based on distance")
+	AlignCam UMETA(DisplayName = "Camera direction alignment"),
+	AlignMove UMETA(DisplayName = "Move direction alignment"),
+	AlignMoveOrCam UMETA(DisplayName = "NON-ZERO move direction alignment or camera direction alignment"),
+	AlignMoveOrDist UMETA(DisplayName = "NON-ZERO move direction alignment or distance"),
+	Dist UMETA(DisplayName = "Distance")
 };
